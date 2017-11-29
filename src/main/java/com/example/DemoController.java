a0dcfb448cfcd77d6ae44c55c91bd582369842d1
Cris <bangomc@gmail.com>
2017-11-29 09:13:48 -0200

    Iniciando

---
+++ src/main/java/com/example/DemoController.java
@@ -0,0 +1,10 @@
+package com.example;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class DemoController {
+	@RequestMapping("/")
+    public String greet() {
+     return "Olha o STF-Programando com Spring boot no GAE!";
+    }
+}
