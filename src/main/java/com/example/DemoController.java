7beeaac971736ed258a7b79adca43a51fa9de82e
Cris <bangomc@gmail.com>
2017-12-19 14:03:29 -0200

    Adicionando RequestBody ao POST

--- src/main/java/com/example/DemoController.java
+++ src/main/java/com/example/DemoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.bean.Lancamento;
@@ -37,7 +38,7 @@ public class DemoController {
 	}
 	
 	@PostMapping("/lancamento")
-	public Lancamento inc(Lancamento lancamento) {
+	public Lancamento inc(@RequestBody Lancamento lancamento) {
 		return lancamentoRepository.save(lancamento);
 	}
 	
